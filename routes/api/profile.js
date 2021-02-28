c330cec2798f30f23c91a23d32be55f5b6a7e758
lkmSasanga <lkmsasanga@gmail.com>
2021-02-28 17:13:54 +0530

    user id kind checking added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -130,7 +130,7 @@ router.get('/user/:user_id', async(req, res) => {
     } catch (err) {
         console.log(err.message)
 
-        if (err.kind == 'ObjectId') {
+        if (err.kind === 'ObjectId') {
             return res.status(400).json({ msg: 'Profile not found!' })
         }
         res.status(500).send('Server Error')
